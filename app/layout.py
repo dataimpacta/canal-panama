94f20cddd010f3251b75fc389307a1f0b8126005
Victor.Munoz <victor.munoz@dataimpacta.com>
2025-02-03 16:51:35 +0200

    Cambio de Git para la app corriendo

--- app/layout.py
+++ app/layout.py
@@ -4,4 +4,6 @@ def create_layout():
     return html.Div([
         html.H1("Hello, Victor, you did it, yay!"),
         html.P("Welcome to my first Dash app! 🎉"),
+        html.P("Ahora acabamos de hacer un cambio y aplicarlo con git 🎉"),
+
     ])
