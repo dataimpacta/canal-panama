731f66deafa71af7ee069ebcfb6fb480a35b9f92
Victor.Munoz <victor.munoz@dataimpacta.com>
2025-06-17 09:43:19 +0200

    Reorganize the title and subtitle

--- app/app.py
+++ app/app.py
@@ -389,4 +389,4 @@ def toggle_chart_modal(open_clicks, close_clicks, is_open):
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run_server(debug=False, host='0.0.0.0', port=8050)
